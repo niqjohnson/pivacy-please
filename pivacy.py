34a3a57e1052ea1b2354ceb7bb7a0c821862dfb6
Nicholas Johnson <niq.johnson@gmail.com>
2016-03-15 22:05:08 -0400

    Add labels

--- pivacy.py
+++ pivacy.py
@@ -13,7 +13,8 @@ yellow = (255,255,0)
 
 #Screen
 pitft_screen_size = (320,240)
-pitft_screen_center = (160,120)
+label_center = (160,90)
+end_time_center = (160,135)
 lcd = pygame.display.set_mode(pitft_screen_size)
 
 #Type
@@ -22,34 +23,34 @@ font_medium = pygame.font.Font(None, 32)
 
 #Status
 def get_status(status_file):
-    status_file_contents = open(status_file).read()
-    status_list = status_file_contents.split()
-    status = status_list[0]
-    end_time = status_list[1]
-    return status, end_time
+    with open(status_file) as status_file_opened:
+        status_file_contents = status_file_opened.read()
+        status_list = status_file_contents.split()
+        status = status_list[0]
+        end_time = status_list[1]
+        return status, end_time
 
 def set_status(status, end_time):
     pygame.mouse.set_visible(False)
     if status == 'free':
         background_color = black
         font_color = white
-        font = font_medium
-        message = 'Next meeting at ' + end_time
-
+        label_content = 'Free until'
     if status == 'working':
         background_color = yellow
         font_color = black
-        font = font_big
-        message = end_time
+        label_content = 'Working until'
     if status == 'busy':
         background_color = red
         font_color = white
-        font = font_big
-        message = end_time
+        label_content = 'Busy until'
     lcd.fill(background_color)
-    display_text = font.render(message, True, font_color)
-    rect = display_text.get_rect(center=pitft_screen_center)
-    lcd.blit(display_text, rect)
+    label_text = font_medium.render(label_content, True, font_color)
+    label_rect = label_text.get_rect(center=label_center)
+    end_time_text = font_big.render(end_time, True, font_color)
+    end_time_rect = end_time_text.get_rect(center=end_time_center)
+    lcd.blit(label_text, label_rect)
+    lcd.blit(end_time_text, end_time_rect)
     pygame.display.update()
 
 while True:
